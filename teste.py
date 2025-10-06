673530d75d95c90d32a666328911bd6ba23ee437
Manuela <manuelahime25@gmail.com>
2025-10-06 15:19:52 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title ('Teste Direito 2')
 st.write("Alô mundo")                                                                                      
-st.image("fala baixo.jpeg", caption="apaguei o hd do pibic. fala baixinho.", width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
+st.image("https://i1.sndcdn.com/artworks-Pdq5tHKgyONPG1gM-nZ21Cw-t500x500.jpg", caption="apaguei o hd do pibic. fala baixinho.", width="content", use_column_width=None, clamp=False, channels="RGB", output_format="auto", use_container_width=None)
